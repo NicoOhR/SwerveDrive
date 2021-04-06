fee0598939c357302d45de053810a3fa2376e00e
NimoOR <nimoohayonr@gmail.com>
2021-04-06 10:32:15 -0500

    extra comments and added ports

--- src/main/java/frc/robot/SwerveDrive.java
+++ src/main/java/frc/robot/SwerveDrive.java
@@ -17,38 +17,52 @@ public class  SwerveDrive {
     }
     //Static b/c drive cries about it
     public void drive(double forward, double strafe, double azimuth) {
+        /*
+         *                FRONT
+         * 
+         *            c          d
+         *            | 		 |
+         *       b ------------------ b
+         *            |          |
+         *            |          |
+         * LEFT       |          |      RIGHT
+         *            |          |
+         *            |          |
+         *       a ------------------ a
+         *            |          |
+         *            c          d
+         * 
+         *                BACK
+         */
         double r = Math.sqrt((L * L) + (W * W));
         azimuth *= -1;
-
+        /*
+        a,b,c, d are vectors, however, they only have the ability to point in one direction, so functionally, they are scalar values
+        forward strafe and azimuth are also technically vector values, all this part is is vector addition 
+        (to mention, strafe * (L/r) is correlative to Omega * r) 
+        */
         double a = forward - strafe * (L / r);
         double b = forward + strafe * (L / r);
         double c = azimuth - strafe * (W / r);
         double d = azimuth + strafe * (W / r);
-
+        //simple pythogras to get the speed between the "vectors"
         double backRightSpeed = Math.sqrt((a * a) + (d * d));
         double backLeftSpeed = Math.sqrt((a * a) + (c * c));
         double frontRightSpeed = Math.sqrt((b * b) + (d * d));
         double frontLeftSpeed = Math.sqrt((b * b) + (c * c));
-        // System.out.println(backRightSpeed);
 
+        //get the unknown angle from the two vectors, IN RADIANS
         double backRightAngle = (Math.atan2(a, d) / Math.PI);
         double backLeftAngle = Math.atan2(a, c) / Math.PI;
         double frontRightAngle = Math.atan2(b, d) / Math.PI;
         double frontLeftAngle = Math.atan2(b, c) / Math.PI;
-        // prints angle in degrees. Calculations are done in radians
-        /*
-         * System.out.println("Back right: " + backRightAngle * 180);
-         * System.out.println("Back left: " + backLeftAngle * 180);
-         * System.out.println("Front left: " + frontLeftAngle * 180);
-         * System.out.println("Front right: " + frontRightAngle * 180);
-         */
+        
 
         backRight.DriveWheel(backRightSpeed, backRightAngle);
         backLeft.DriveWheel(backLeftSpeed,backLeftAngle);
         frontRight.DriveWheel(frontRightSpeed,frontRightAngle);
         frontLeft.DriveWheel(frontLeftSpeed,frontLeftAngle);
     }
-    //making it non static for now, still dont know what it'll do
     public void driveSimple(double speed, double angle){
         backRight.DriveWheel(speed, angle);
 		backLeft.DriveWheel(speed, angle);
